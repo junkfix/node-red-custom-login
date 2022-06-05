068cd83b79437886fce7ef8372cf877bf54d1eac
htmltiger <1429451+htmltiger@users.noreply.github.com>
2022-06-05 18:53:21 +0100

    Update custom-login.js

--- custom-login.js
+++ custom-login.js
@@ -27,25 +27,29 @@ function customLogin(req, res, next) {
 	}
 	*/
 	
-	
-	if(!(ip in ipban)){ipban[ip]=0;}
 	if (req.headers.cookie) {
+		if(!(ip in ipban)){ipban[ip]={lastf:0,attempts:0};}
+		
+		//reset every 15mins 
+		if(ipban[ip].attempts>0 && Date.now() - ipban[ip].lastf >(15 * 60000)){ipban[ip].attempts=0;}
+		
 		logy = cookie.parse(req.headers.cookie);
 		logy = logy["log"];
 		if(logy){
 			if(logy == myhash){
 				return next();
-			}else if(Date.now() - ipban[ip] > (5 * 60000) && //5m one login
+			}else if(ipban[ip].attempts < 3 && //max 3 attempts in 15mins
 				crypto.createHash('sha512').update('#'+logy, 'utf8').digest('hex')==myhash)
 				{					
-				mqttmsg("new: "+ip,req.headers);
+				alertme("PASSED: "+ip);
 				res.cookie("log", myhash, {expires: new Date(Date.now() + (14 * 864e5)), path: '/'}); //remember for 14 days
 				delete ipban[ip];
 				return res.redirect(req.originalUrl);
 			}else{
-				mqttmsg("FAILED: "+ip+" "+logy,req.headers);
+				alertme("FAILED: "+ip+" "+logy,req.headers);
 				res.clearCookie("log");
-				ipban[ip]=Date.now();
+				ipban[ip].lastf=Date.now();
+				ipban[ip].attempts++;
 				logy="Login Failed";
 				code=401;
 			}
@@ -54,18 +58,26 @@ function customLogin(req, res, next) {
 	}
 	
 	res.writeHeader(code, {"Content-Type": "text/html"})
-	res.write('<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1" /><style>body{background:#3498DB}form{margin:20px auto;width:300px;background:#fff;border-radius:5px;padding:20px;text-align:center;color:red}input{padding:12px 0;margin-bottom:10px;border-radius:3px;border:1px solid transparent;text-align:center;width:100%;font-size:16px}.f{background:#ECF0F1}.f:focus{border:1px solid #3498DB}.b{background:#3498DB;color:#fff}.b:hover,.b:active{background:#1F78B4}</style></head><body><form method="POST" onsubmit="document.cookie=\'log=\' + this.querySelector(\'#u\').value+\'-\'+this.querySelector(\'#p\').value+\'; path=/\'; return true;"><input type="text" placeholder="username" id="u" name="username" class="f"><input type="password" placeholder="password" id="p" name="password" class="f"><input type="submit" value="login" class="b">'+logy+'</form></body></html>');
+	res.write('<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1" /><style>body{background:#3498DB}');
+	res.write('form{margin:20px auto;width:300px;background:#fff;border-radius:5px;padding:20px;text-align:center;color:red}input{padding:12px 0;');
+	res.write('margin-bottom:10px;border-radius:3px;border:1px solid transparent;text-align:center;width:100%;font-size:16px}.f{background:#ECF0F1}');
+	res.write('.f:focus{border:1px solid #3498DB}.b{background:#3498DB;color:#fff}.b:hover,.b:active{background:#1F78B4}</style></head><body><form ');
+	res.write('method="POST" onsubmit="document.cookie=\'log=\'+this.querySelector(\'#u\').value+\'-\'+this.querySelector(\'#p\').value+\'; ');
+	res.write('path=/\';return true;"><input type="text" placeholder="Username" id="u" name="username" class="f"><input type="password" ');
+	res.write('placeholder="Password" id="p" name="password" class="f"><input type="submit" value="Login" class="b">'+logy+'</form></body></html>');
 	return res.end();
 
 }
 
 
-function mqttmsg(msg, headers){
-	headers=JSON.stringify(headers,null,2);
-	msg="nodelogin: "+msg
+function alertme(msg, headers){
+	if(headers){headers=JSON.stringify(headers,null,2);}else{headers='';}
+	msg="nodelogin: "+msg;
 	console.warn("[warn] "+msg);
+	
+	//for custom shell command
 	//const { execFile } = require('node:child_process');
-	//execFile('mosquitto_pub', ["-h","localhost","-c","-u","mqttusername","-P","mqttpassword","-t","yourtopic", "-m", msg+"\n"+headers], (error, stdout, stderr) => {});
+	//execFile('mosquitto_pub', ["-h","localhost","-c","-u","mqttusername","-P","mqttpassword","-t","yourtopic", "-m", msg+"\n"+headers]);
 }
 
 module.exports = customLogin;
